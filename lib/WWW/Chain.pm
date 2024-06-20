6d2043cc3997f07b8cd074ebe062f70ad9b25e22
Torsten Raudssus <torsten@raudss.us>
2024-06-20 02:25:17 +0200

    Fixing intendation, removing .travis.yml

--- lib/WWW/Chain.pm
+++ lib/WWW/Chain.pm
@@ -66,53 +66,53 @@ use Safe::Isa;
 use WWW::Chain::UA::LWP;
 
 has stash => (
-	isa => HashRef,
-	is => 'lazy',
+  isa => HashRef,
+  is => 'lazy',
 );
 
 sub _build_stash {{}}
 
 has next_requests => (
-	isa => ArrayRef,
-	is => 'rwp',
-	clearer => 1,
-	required => 1,
+  isa => ArrayRef,
+  is => 'rwp',
+  clearer => 1,
+  required => 1,
 );
 
 has next_coderef => (
-	#isa => CodeRef,
-	is => 'rwp',
-	clearer => 1,
+  #isa => CodeRef,
+  is => 'rwp',
+  clearer => 1,
 );
 
 has done => (
-	isa => Bool,
-	is => 'rwp',
-	default => sub { 0 },
+  isa => Bool,
+  is => 'rwp',
+  default => sub { 0 },
 );
 
 has request_count => (
-	isa => Num,
-	is => 'rwp',
-	default => sub { 0 },
+  isa => Num,
+  is => 'rwp',
+  default => sub { 0 },
 );
 
 has result_count => (
-	isa => Num,
-	is => 'rwp',
-	default => sub { 0 },
+  isa => Num,
+  is => 'rwp',
+  default => sub { 0 },
 );
 
 sub BUILDARGS {
-	my $self = shift;
-	return $_[0] if (scalar @_ == 1 && ref $_[0] eq 'HASH');
-	my ( $next_requests, $next_coderef, @args ) = $self->parse_chain(@_);
-	return {
-		next_requests => $next_requests,
-		next_coderef => $next_coderef,
-		request_count => scalar @{$next_requests},
-		@args,
-	};
+  my $self = shift;
+  return $_[0] if (scalar @_ == 1 && ref $_[0] eq 'HASH');
+  my ( $next_requests, $next_coderef, @args ) = $self->parse_chain(@_);
+  return {
+    next_requests => $next_requests,
+    next_coderef => $next_coderef,
+    request_count => scalar @{$next_requests},
+    @args,
+  };
 }
 
 sub request_with_lwp {
@@ -121,50 +121,50 @@ sub request_with_lwp {
 }
 
 sub parse_chain {
-	my ( $self, @args ) = @_;
-	my $coderef;
-	my @requests;
-	while (@args) {
-		my $arg = shift @args;
-		if ( $arg->$_isa('HTTP::Request') ) {
-			push @requests, $arg;
-		} elsif (ref $arg eq 'CODE') {
-			$coderef = $arg;
-			last;
-		} elsif (ref $arg eq '') {
-			die "".(ref $self)."->parse_chain '".$arg."' is not a known function" unless $self->can($arg);
-			$coderef = $arg;
-			last;
-		} else {
-			die "".(ref $self)."->parse_chain got unparseable element".(defined $arg ? " ".$arg : "" );
-		}
-	}
-	die "".(ref $self)."->parse_chain found no HTTP::Request objects" unless @requests;
-	return [@requests], $coderef, @args;
+  my ( $self, @args ) = @_;
+  my $coderef;
+  my @requests;
+  while (@args) {
+    my $arg = shift @args;
+    if ( $arg->$_isa('HTTP::Request') ) {
+      push @requests, $arg;
+    } elsif (ref $arg eq 'CODE') {
+      $coderef = $arg;
+      last;
+    } elsif (ref $arg eq '') {
+      die "".(ref $self)."->parse_chain '".$arg."' is not a known function" unless $self->can($arg);
+      $coderef = $arg;
+      last;
+    } else {
+      die "".(ref $self)."->parse_chain got unparseable element".(defined $arg ? " ".$arg : "" );
+    }
+  }
+  die "".(ref $self)."->parse_chain found no HTTP::Request objects" unless @requests;
+  return [@requests], $coderef, @args;
 }
 
 sub next_responses {
-	my ( $self, @responses ) = @_;
-	die "".(ref $self)."->next_responses can't be called on chain which is done." if $self->done;
-	my $amount = scalar @{$self->next_requests};
-	die "".(ref $self)."->next_responses would need ".$amount." HTTP::Response objects to proceed"
-		unless scalar @responses == $amount;
-	die "".(ref $self)."->next_responses only takes HTTP::Response objects"
-		if grep { !$_->isa('HTTP::Response') } @responses;
-	$self->clear_next_requests;
-	my @result = $self->${\$self->next_coderef}(@responses);
-	$self->clear_next_coderef;
-	$self->_set_result_count($self->result_count + 1);
-	if ( $result[0]->$_isa('HTTP::Request') ) {
-		my ( $next_requests, $next_coderef, @args ) = $self->parse_chain(@result);
-		die "".(ref $self)."->next_responses can't parse the result, more arguments after CodeRef" if @args;
-		$self->_set_next_requests($next_requests);
-		$self->_set_next_coderef($next_coderef);
-		$self->_set_request_count($self->request_count + scalar @{$next_requests});
-		return 0;
-	}
-	$self->_set_done(1);
-	return $self->stash;
+  my ( $self, @responses ) = @_;
+  die "".(ref $self)."->next_responses can't be called on chain which is done." if $self->done;
+  my $amount = scalar @{$self->next_requests};
+  die "".(ref $self)."->next_responses would need ".$amount." HTTP::Response objects to proceed"
+    unless scalar @responses == $amount;
+  die "".(ref $self)."->next_responses only takes HTTP::Response objects"
+    if grep { !$_->isa('HTTP::Response') } @responses;
+  $self->clear_next_requests;
+  my @result = $self->${\$self->next_coderef}(@responses);
+  $self->clear_next_coderef;
+  $self->_set_result_count($self->result_count + 1);
+  if ( $result[0]->$_isa('HTTP::Request') ) {
+    my ( $next_requests, $next_coderef, @args ) = $self->parse_chain(@result);
+    die "".(ref $self)."->next_responses can't parse the result, more arguments after CodeRef" if @args;
+    $self->_set_next_requests($next_requests);
+    $self->_set_next_coderef($next_coderef);
+    $self->_set_request_count($self->request_count + scalar @{$next_requests});
+    return 0;
+  }
+  $self->_set_done(1);
+  return $self->stash;
 }
 
 1;
--- lib/WWW/Chain/UA/LWP.pm
+++ lib/WWW/Chain/UA/LWP.pm
@@ -4,27 +4,26 @@ package WWW::Chain::UA::LWP;
 use Moo;
 extends 'LWP::UserAgent';
 
-use HTTP::Cookies;
-
 with qw( WWW::Chain::UA );
 
+use HTTP::Cookies;
 use Scalar::Util 'blessed';
 use Safe::Isa;
 
 sub request_chain {
-	my ( $self, $chain ) = @_;
-	die __PACKAGE__."->request_chain needs a WWW::Chain object as parameter"
-		unless ( blessed($chain) && $chain->$_isa('WWW::Chain') );
-	$self->cookie_jar({}) unless $self->cookie_jar;
-	while (!$chain->done) {
-		my @responses;
-		for (@{$chain->next_requests}) {
-			my $response = $self->request($_);
-			push @responses, $response;
-		}
-		$chain->next_responses(@responses);
-	}
-	return $chain;
+  my ( $self, $chain ) = @_;
+  die __PACKAGE__."->request_chain needs a WWW::Chain object as parameter"
+    unless ( blessed($chain) && $chain->$_isa('WWW::Chain') );
+  $self->cookie_jar({}) unless $self->cookie_jar;
+  while (!$chain->done) {
+    my @responses;
+    for (@{$chain->next_requests}) {
+      my $response = $self->request($_);
+      push @responses, $response;
+    }
+    $chain->next_responses(@responses);
+  }
+  return $chain;
 }
 
 1;
